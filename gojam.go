2c9fdc19026e5c1db45e9e2a30d9752026c7ffc7
Matt Katz <mykatz01@gmail.com>
2019-06-26 01:37:46 -0700

    I think this works but there's probably a bug

--- gojam.go
+++ gojam.go
@@ -1,1 +1,55 @@
 package gojam
+
+import (
+	"fmt"
+	"strings"
+)
+
+//These signal the start and end of a sentence.
+const (
+	StartToken = "_START_"
+	EndToken   = "_END_"
+)
+
+type Markov struct {
+	n         int    //the 'n' in n-gram, how long
+	separator string //what separates each gram - for words, usually spaces.
+	chain     map[string]*Output
+}
+
+type Output struct {
+	occurrences int            //how many times the 'input' occurs. ie total of all output possibilities = 100%
+	grams       map[string]int //how many times the word appears AFTER the input
+}
+
+func NewMarkov(grams int, separator string) *Markov {
+	m := Markov{grams, separator, make(map[string]*Output)}
+	return &m
+}
+
+func (m Markov) TrainOnExample(sentence string) { //for single example
+	queue := []string{}
+	for i := range queue {
+		queue[i] = ""
+	}
+	queue[len(queue)-1] = "_START_"
+	words := strings.Fields(sentence)
+	for i := range words {
+		st := words[i]
+		prefix := strings.Join(queue, " ")
+		opt := m.chain[prefix]
+		if opt == nil {
+			m.chain[prefix] = &Output{0, make(map[string]int)}
+			opt = m.chain[prefix]
+		}
+		opt.grams[st] += 1
+		//dequeue first element and shift everything
+		queue[0] = "" //I've heard this helps with memory
+		queue = queue[1:]
+		queue = append(queue, st)
+	}
+}
+
+func (m Markov) PrintMap() {
+	fmt.Println(m.chain)
+}
